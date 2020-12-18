ee7e8312c312df79d22f236a35644f02b062b71c
matus1888 <matus1888@gmail.com>
2020-12-18 20:19:41 +0300

    started adding  responsive layout in this calc-app

--- src/components/memMenu/MemBuffer.jsx
+++ src/components/memMenu/MemBuffer.jsx
@@ -16,18 +16,16 @@ class MemBuffer extends React.Component{
             wrapper.classList.remove(s.bufferActive)
         }
     }
-    getSnapshotBeforeUpdate(prevProps, prevState) {
+    componentDidUpdate(prevProps, prevState) {
         localStorage.setItem('memBufferHistory',JSON.stringify(this.props.mCell))
-        return null;
     }
 
     render() {
         this.handleClick()
         return (
-            <div ref={this.wrapperRef} className={s.buffer}>{this.props.mCell}</div>
+            <div ref={this.wrapperRef} className={s.buffer}>{this.props.mCell}
+            </div>
         )
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
     }
 
 }
