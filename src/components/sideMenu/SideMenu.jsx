7ec2cba9bd08821c4c1003c462f2db4a32c3db0e
matus1888 <matus1888@gmail.com>
2020-12-27 21:29:56 +0300

    edit error of font import in side menu

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -23,26 +23,26 @@ class SideMenu extends React.Component{
                     <div className={s.header}></div>
                     <div className={s.body}>
                         <div className={s.d}>Калькулятор</div>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE8EF; <span className={s.fontSegoeUI}>Обычный</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF196; <span className={s.fontSegoeUI}>Инженерный</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xECCE; <span className={s.fontSegoeUI}>Программист</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE787; <span className={s.fontSegoeUI}>Вычисление Даты</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE8EF; <span className={s.fontSeguiSym}>Обычный</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF196; <span className={s.fontSeguiSym}>Инженерный</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xECCE; <span className={s.fontSeguiSym}>Программист</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE787; <span className={s.fontSeguiSym}>Вычисление Даты</span></button>
                         <div className={s.d}>Преобразователь</div>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xEB0D;  <span className={s.fontSegoeUI}>Валюта</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF1AA;  <span className={s.fontSegoeUI}>Объем</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xECC6;  <span className={s.fontSegoeUI}>Длина</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF4C1;  <span className={s.fontSegoeUI}>Вес и масса</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE7A3;  <span className={s.fontSegoeUI}>Температура</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xECAD;  <span className={s.fontSegoeUI}>Энергия</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE809;  <span className={s.fontSegoeUI}>Площадь</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xEADA;  <span className={s.fontSegoeUI}>Скорость</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE917;  <span className={s.fontSegoeUI}>Время</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE945;  <span className={s.fontSegoeUI}>Мощность</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF20F;  <span className={s.fontSegoeUI}>Данные</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xEC4A;  <span className={s.fontSegoeUI}>Давление</span></button>
-                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF515;  <span className={s.fontSegoeUI}>Угол</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xEB0D;  <span className={s.fontSeguiSym}>Валюта</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF1AA;  <span className={s.fontSeguiSym}>Объем</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xECC6;  <span className={s.fontSeguiSym}>Длина</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF4C1;  <span className={s.fontSeguiSym}>Вес и масса</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE7A3;  <span className={s.fontSeguiSym}>Температура</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xECAD;  <span className={s.fontSeguiSym}>Энергия</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE809;  <span className={s.fontSeguiSym}>Площадь</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xEADA;  <span className={s.fontSeguiSym}>Скорость</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE917;  <span className={s.fontSeguiSym}>Время</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xE945;  <span className={s.fontSeguiSym}>Мощность</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF20F;  <span className={s.fontSeguiSym}>Данные</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xEC4A;  <span className={s.fontSeguiSym}>Давление</span></button>
+                        <button className={`${s.b} ${s.fontWinCalc}`}>&#xF515;  <span className={s.fontSeguiSym}>Угол</span></button>
                     </div>
-                    <button className={`${s.footer} ${s.fontWinCalc}`}>&#xE946;  <span className={s.fontSegoeUI}>  О программе</span></button>
+                    <button className={`${s.footer} ${s.fontWinCalc}`}>&#xE946;  <span className={s.fontSeguiSym}>  О программе</span></button>
                  </div>
         )
     }
