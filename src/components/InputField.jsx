93c167dff9374157628a46ae5e53813ae0d028cc
Alex <39013396+matus1888@users.noreply.github.com>
2021-06-21 10:31:56 +0300

    Delete src directory

--- src/components/InputField.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const InputField = (props) => {
-    return (<input className={props.className} value={props.base.currentValue} readOnly={true}>
-    </input>)
-}
-export default InputField;
